75f1075f946dbcc11fe0d72a585d738cbd644614
Иван Сташко <4sv.exe@gmail.com>
2023-07-24 23:43:50 +0300

    fix some responsibility & styles

--- src/app/db/fakeExchange.ts
+++ src/app/db/fakeExchange.ts
@@ -3,7 +3,7 @@ import { ExchangeInterface } from './../interfaces/index';
 export const fakeExchange: ExchangeInterface = {
   r030: 0,
   txt: "loading...",
-  rate: 0,
+  rate: 1,
   cc: "loading...",
   exchangedate: "loading...",
 };
